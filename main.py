a32c98a2a3ebde78345144e66e9066e099d154a1
cjamieson88 <chris.m.jamieson@gmail.com>
2023-02-03 07:24:43 -0800

    Add Deck class and make_deck function

--- main.py
+++ main.py
@@ -18,3 +18,41 @@ class Dice:
     def roll_series(self, rolls):
         values = random.choices(self.population, self.weights, k=rolls)
         return values
+
+
+class Deck:
+    def __init__(self, number_of_jokers=0, composition=None):
+        self.number_of_jokers = number_of_jokers
+        self.composition = composition
+
+        if composition is None:
+            self.composition = make_deck(self.number_of_jokers)
+
+    def draw(self, number_of_cards=1):
+        cards = random.choices(self.composition, k=number_of_cards)
+
+        for card in cards:
+            self.composition.remove(card)
+
+        if number_of_cards == 1:
+            return cards[0]
+        else:
+            return cards
+
+    def get_composition(self):
+        return self.composition
+
+
+def make_deck(jokers=0):
+    deck = []
+    for suit in ["S", "D", "C", "H"]:
+        deck.append(f"A{suit}")
+        for number in range(2, 10):
+            deck.append(f"{number}{suit}")
+        for face in ["T", "J", "Q", "K"]:
+            deck.append(f"{face}{suit}")
+
+    for number in range(jokers):
+        deck.append(f"J{number}")
+
+    return deck
